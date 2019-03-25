ec9cdedf809429cc802d349ffdbfef056ed42686
Kibian <kevcass01@yahoo.fr>
2019-03-25 11:37:24 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Travel from "./Travel";
 
 class App extends Component {
   render() {
@@ -8,18 +10,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Travel</h1>
         </header>
+        <Travel
+          destination="France"
+          country="Lyon"
+          photo="https://image.noelshack.com/fichiers/2019/13/1/1553509651-lyon.jpg"
+          distance=" no-distance"
+        />
+        <Travel
+          destination="Borneo"
+          country="Sarawak"
+          photo="https://image.noelshack.com/fichiers/2019/13/1/1553509254-wm-jungle-waterfall-min.jpg"
+          distance=" 11.401,73 km"
+        />
       </div>
     );
   }
